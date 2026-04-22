15c05beed93f97054f52b14354bb0b7f32f12892
Doumbia Mohamed <doumbiamohamedassorb@gmail.com>
2026-04-22 11:35:37 +0200

    two commits

--- age.py
+++ age.py
@@ -5,7 +5,7 @@ elif 8 <= age <= 9:
     print("il s'agit d'une categorie de Pupille")
 elif 10 <= age <= 11:
     print("Il s'agit d'une categorie Minime")
-elif age > 12:
+elif age >=12:
     print("Il s'agit d'un cadet")
 else:
     print(" vous n'etes pas dans cette categories")
